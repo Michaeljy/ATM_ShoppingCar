6a89f1155191feeae3301c51b64852eb4f347ea2
Micheal bennington <49580254+Michaeljy@users.noreply.github.com>
2019-08-25 14:53:54 +0800

    Update bank.py

--- interface/bank.py
+++ interface/bank.py
@@ -21,3 +21,30 @@ def transfer_interface(from_username, to_username, money):# 转账接口，
         db_handler.save_json(from_user_dic) #存数据
         db_handler.save_json(to_user_dict) #存数据
         return msg_f #返回操作信息
+def repay_interface(username): #还款接口
+    while True:
+        money = input("请输入你的还款金额：").strip()
+        if not money.isdigit():
+            print('请输入数字')
+        else:
+            money = int(money)
+            user_dic = db_handler.read_json(username) #拿用户数据
+            user_dic['extra'] += money # 字典里面加钱
+            db_handler.save_json(user_dic) #保存数据
+            return f'{username}已成功还款{money}' #返回信息
+
+
+def withdraw_interface(username, money):# 取现接口
+    user_dic = db_handler.read_json(username) #拿用户信息
+
+    if money*1.005 > user_dic['extra']:
+        return "钱不够"
+    else:
+        user_dic['extra'] -= money*1.05 # 减钱
+        db_handler.save_json(user_dic) # 存数据 
+        return f'{username}已成功取现{money}元'
+
+
+def bank_flow_interface(username): # 银行流水信息
+    user_dic = db_handler.read_json(username) # 拿用户数据
+    return user_dic['bank_flow'] #返回用户字典里的流水
